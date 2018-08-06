36ba609b77cc79f5508bd18e100d5dbde6e48955
benxiaohai <zhangyc_java@163.com>
2018-08-06 22:40:28 +0800

    更改环境配置，可以正常启动

--- config/index.js
+++ config/index.js
@@ -11,11 +11,11 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/': {
-          target: 'http://localhsot:8090',
+      '/admin': {
+          target: 'http://127.0.0.1:8090',
           changeOrigin: true,
           pathRewrite: {
-              '^/': '/'
+              '^/admin': '/'
           }
         }
     },
--- src/utils/eventHandler.js
+++ src/utils/eventHandler.js
@@ -1,4 +1,4 @@
-import Vue from 'Vue'
+import Vue from 'vue'
 import {getRequest} from '../utils/api'
 Vue.prototype.getRequest = getRequest;
 export default new Vue();
